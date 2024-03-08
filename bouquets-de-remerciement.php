cfbae739b679986d7742582607010187cd3a9119
Nouur2 <162565248+Nouur2@users.noreply.github.com>
2024-03-09 00:13:23 +0100

    Create bouquets-de-remerciement.php

---
+++ bouquets-de-remerciement.php
@@ -0,0 +1,32 @@
+<?php
+$pageTitle = "Bouquet de remerciements";
+include 'include/header.inc.php';
+?>
+
+<?php
+// Lire le fichier JSON
+$jsonData = file_get_contents('data.json');
+// Décoder le JSON
+$data = json_decode($jsonData, true);
+
+// Vérifier si la catégorie existe
+if (isset($data['Bouquets de remerciement'])) {
+    echo "<div id='categories-container'>";
+    foreach ($data['Bouquets de remerciement'] as $bouquet) {
+        echo "<div class='product-card'>";
+        echo "<img class='product-image' src='" . htmlspecialchars($bouquet['image_link']) . "' alt='Image'>";
+        echo "<div class='product-info'>";
+        echo "<h2 class='product-name'>" . htmlspecialchars($bouquet['name']) . "</h2>";
+        echo "<p class='product-description'>" . htmlspecialchars($bouquet['description']) . "</p>";
+        echo "<p class='product-price'>Prix Moyen: " . htmlspecialchars($bouquet['prices']['moyen']) . "€ - Prix Grand: " . htmlspecialchars($bouquet['prices']['grand']) . "€</p>";
+        echo "<div class='product-action'>";
+        echo "<input type='number' value='1' min='1' class='quantity-input'>";
+        echo "<button class='add-to-cart'>Ajouter au panier</button>";
+        echo "</div>"; // Fermeture de product-action
+        echo "</div>"; // Fermeture de product-info
+        echo "</div>"; // Fermeture de product-card
+    }
+    echo "</div>"; // Fermeture de categories-container
+    }
+
+?>
