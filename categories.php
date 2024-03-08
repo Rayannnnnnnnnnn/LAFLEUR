133c6066f9383ef4d8903f162a84cba01a1c42bc
Nouur2 <162565248+Nouur2@users.noreply.github.com>
2024-03-09 00:16:42 +0100

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,14 @@
+<?php
+$pageTitle = "Nos catégories de Bouquets";
+include 'include/header.inc.php';
+?>
+
+<div id="categories-container"></div>
+ 
+ <script src="js/categories.js"></script>
+     <footer>
+        
+     </footer>
+    
+ </body>
+ </html>
